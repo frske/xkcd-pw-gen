1409f5f9585c4a7aae3746cfceba6327f95d9778
frske <144455292+frske@users.noreply.github.com>
2023-10-21 17:28:16 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Simple xkcd inspired password generator");
+        Console.WriteLine("Press any key to continue"); 
+        Console.ReadKey();
+        Console.Clear();
+
+        string filePath = "./wordsn.txt";
+        string[] lines = File.ReadAllLines(filePath); 
+
+        Random random =  new Random(); 
+
+        for (var i = 0; i < 4; i++)
+        {
+            
+        }
+
+        int rndVal1 = random.Next(0, lines.Length); 
+        int rndVal2 = random.Next(0, lines.Length);
+        int rndVal3 = random.Next(0, lines.Length); 
+        int rndVal4 = random.Next(0, lines.Length);
+
+        Console.WriteLine(lines[rndVal1] + "-" + lines[rndVal2] + "-" + lines[rndVal3] + "-" + lines[rndVal4]);
+
+        //string line = File.ReadLines(filePath).ElementAt(36);
+        //Console.WriteLine(line);
+
+
+
+
+    }
+}
---
+++ RemoveWords.cs
@@ -0,0 +1,27 @@
+using System.Runtime.InteropServices;
+
+public class RemoveWords
+{
+    public static void WordsRemover()
+    {   
+        //string of where the words are located and reads them
+        string pathWords = "./wordsduped.txt";
+        string[] lines = File.ReadAllLines(pathWords);
+        
+        string filteredWordsPath = "./wordsn.txt";
+
+        //checks if words is less than 4, writes it to new file otherwise
+        foreach (string line in lines)
+        {
+            if (line.Length > 4)
+            {
+                File.AppendAllText(filteredWordsPath, line + "\n"); 
+            }
+            else
+            {
+                continue; 
+            }
+        }
+    }
+}
+
