626c0b5ed5601c11c42b7b2fa68ece31ccf95d89
frske <144455292+frske@users.noreply.github.com>
2023-10-22 11:37:34 +0200

    rewrite asks for user input, likely more efficiet

--- Program.cs
+++ Program.cs
@@ -1,33 +1,34 @@
 ﻿using System.IO;
+using System.Diagnostics;
 public class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
         Console.Clear();
         Console.WriteLine("Simple xkcd inspired password generator");
+        Console.WriteLine("https://xkcd.com/936/"); 
         Console.WriteLine("Press any key to continue"); 
         Console.ReadKey();
         Console.Clear();
 
-        string filePath = "./wordsn.txt";
-        string[] lines = File.ReadAllLines(filePath); 
+        //RemoveWords.WordsRemover(); 
+
+        Console.Write("How many words should your password contain (default 4): ");
+        int option = 4;
+        option = Convert.ToInt32(Console.ReadLine());
+        
+        //typecheck
 
         Random random =  new Random(); 
+        string filePath = "./wordlists/wordsn.txt"; //filepath of location of words
+        string[] lines = File.ReadAllLines(filePath); //loads file into memory ~15mb
+        int[] randomValues = new int[option]; //array to hold random values, as many as the user requested
 
-        for (var i = 0; i < 4; i++)
+        for (int i = 0; i < option; i++) //generate random values and puts them into the array
         {
-            
+            randomValues[i] = random.Next(0, lines.Length);
+            Debug.WriteLine("index " + i + ": " + randomValues[i]);
+            Console.Write(lines[randomValues[i]] + "-");
         }
-
-        int rndVal1 = random.Next(0, lines.Length); 
-        int rndVal2 = random.Next(0, lines.Length);
-        int rndVal3 = random.Next(0, lines.Length); 
-        int rndVal4 = random.Next(0, lines.Length);
-
-        Console.WriteLine(lines[rndVal1] + "-" + lines[rndVal2] + "-" + lines[rndVal3] + "-" + lines[rndVal4]);
-
-        //string line = File.ReadLines(filePath).ElementAt(36);
-        //Console.WriteLine(line);
-
     }
 }
