1385d5c5270e84f10a18d5fbee91f391044abe7d
frske <144455292+frske@users.noreply.github.com>
2023-10-22 11:55:00 +0200

    check if input is valid + graphical improvements

--- Program.cs
+++ Program.cs
@@ -13,22 +13,43 @@ public class Program
 
         //RemoveWords.WordsRemover(); 
 
-        Console.Write("How many words should your password contain (default 4): ");
         int option = 4;
-        option = Convert.ToInt32(Console.ReadLine());
-        
-        //typecheck
 
+        bool validInput = false; 
+
+        while (!validInput)
+        {   
+            Console.Clear();
+            Console.Write("How many words should your password contain (default 4): ");
+            var input = Console.ReadLine();
+
+            validInput = int.TryParse(input, out option);
+
+            if (option <= 0 || validInput == false)
+            {   
+                Console.Clear();
+                Console.WriteLine("ERROR: INVALID INPUT");
+                Console.WriteLine("Press any key to continue"); 
+                validInput = false;
+                Console.ReadKey();
+            }
+        }
+        
         Random random =  new Random(); 
         string filePath = "./wordlists/wordsn.txt"; //filepath of location of words
         string[] lines = File.ReadAllLines(filePath); //loads file into memory ~15mb
         int[] randomValues = new int[option]; //array to hold random values, as many as the user requested
 
-        for (int i = 0; i < option; i++) //generate random values and puts them into the array
+        for (int i = 0; i < option; i++) 
         {
-            randomValues[i] = random.Next(0, lines.Length);
-            Debug.WriteLine("index " + i + ": " + randomValues[i]);
-            Console.Write(lines[randomValues[i]] + "-");
+            randomValues[i] = random.Next(0, lines.Length); //generate random values and puts them into the array
+            Debug.WriteLine("index " + i + ": " + randomValues[i]); //debug output if the numbers are generating correctly
+            Console.Write(lines[randomValues[i]]); //writes the random words
+            if (i < (option - 1)) //adds the "-" in between words
+            {
+                Console.Write("-");
+            }
+            
         }
     }
 }
